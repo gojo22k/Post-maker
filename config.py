f482450ba9ac3bd31e87ac95df5e091eec6dd79c
Ankit <155639266+gojo22k@users.noreply.github.com>
2025-10-20 11:59:23 +0530

    Add APP_URL environment variable to config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 # Load from environment variables in Koyeb
+APP_URL = os.getenv("APP_URL", "https://aniflixofficials.koyeb.app")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 API_ID = os.getenv("API_ID", "")
 API_HASH = os.getenv("API_HASH", "")
